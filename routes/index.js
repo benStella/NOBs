5f7ecce923fbacd5f4a47ab267659ccd4729f3b2
Ben <blaze0013013@gmail.com>
2022-05-24 08:47:25 -0400

    removed server

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const htmlRoutes = require('./htmlRoutes')
-const contactRoutes = require('./contactRoutes')
-
-router.use(htmlRoutes)
-router.use(contactRoutes)
-
-module.exports = router
